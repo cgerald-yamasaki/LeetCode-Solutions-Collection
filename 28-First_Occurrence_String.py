943187df329fa6fc926c265b05306858cb2adfea
cgerald-yamasaki <94801618+cgerald-yamasaki@users.noreply.github.com>
2023-03-03 17:16:37 -0800

    first pass at finding first needle string in haystack string

--- 28-First_Occurrence_String.py
+++ 28-First_Occurrence_String.py
@@ -1,1 +1,22 @@
+# Find the Index of the First Occurrence in a String
+
+# Given two strings needle and haystack, return the index of the
+# first occurrence of needle in haystack, or -1 if needle is not
+# part of haystack.
+
+# works, gets either 90.91st percentile or 78.97th for runtime..., 54.39th for memory
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        hay_len = len(haystack)
+        needle_len = len(needle)
+        if needle_len > hay_len: return -1
+        for h_index in range(hay_len - (needle_len - 1)):
+            for n_index in range(needle_len):
+                if n_index == needle_len - 1 and haystack[h_index + n_index] == needle[n_index]:
+                    return h_index
+                if haystack[h_index + n_index] != needle[n_index]:
+                    break
+        return -1
+
+
 
