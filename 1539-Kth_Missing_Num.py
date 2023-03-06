984eb224ec4c857b42ce64ce316e5fb4eba45b50
cgerald-yamasaki <94801618+cgerald-yamasaki@users.noreply.github.com>
2023-03-06 10:57:16 -0800

    first pass

--- 1539-Kth_Missing_Num.py
+++ 1539-Kth_Missing_Num.py
@@ -1,1 +1,19 @@
+# Kth Missing Positive Number
 
+# Given an array arr of positive integers sorted in a strictly
+# increasing order, and an integer k.
+# Return the kth positive integer that is missing from this array.
+
+# works, 88.93rd percentile for runtime, 80.43rd for memory
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        missing_count = 0
+        curr = 1
+        for num in arr:
+            while curr < num:
+                missing_count += 1
+                if missing_count == k:
+                    return curr
+                curr += 1
+            curr += 1
+        return curr + (k - missing_count - 1)
