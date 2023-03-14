09de4286db7d4f1a776340865126fedaa317cf1b
cgerald-yamasaki <94801618+cgerald-yamasaki@users.noreply.github.com>
2023-03-13 22:35:11 -0700

    using recursion instead of queue

--- 101-Symmetric_Tree.py
+++ 101-Symmetric_Tree.py
@@ -9,24 +9,47 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-# works, ~35.42nd percentile for runtime, 14.61st for memory
+
+# alternate solution, with some guidance from posted solutions
+# works, ~54.96th percentile for runtime, 52.37th for memory
+# ... or 92.87th for runtime...
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        if root == None: return True
-        node1 = root
-        node2 = root
-        queue = [root, root]
-        while queue != []:
-            node1 = queue.pop(0)
-            node2 = queue.pop(0)
+        def checkTwo(node1: Optional[TreeNode], node2: Optional[TreeNode]) -> bool:
+            if node1 == None and node2 == None:
+                return True
+            if node1 == None or node2 == None:
+                return False
             if (node1.left == None and node2.right != None) or (node1.right == None and node2.left != None):
                 return False
             if node1.left != None and node2.right != None:
                 if node1.left.val != node2.right.val: return False
-                queue.append(node1.left)
-                queue.append(node2.right)
             if node1.right != None and node2.left != None:
                 if node1.right.val != node2.left.val: return False
-                queue.append(node1.right)
-                queue.append(node2.left)
-        return True
+            if checkTwo(node1.left, node2.right) and checkTwo(node1.right, node2.left):
+                return True
+            else: return False
+        return checkTwo(root, root)
+
+# works, ~35.42nd percentile for runtime, ~14.61st for memory
+# ... or 16.41st for runtime and 90.47th for memory...
+# class Solution:
+#     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+#         if root == None: return True
+#         node1 = root
+#         node2 = root
+#         queue = [root, root]
+#         while queue != []:
+#             node1 = queue.pop(0)
+#             node2 = queue.pop(0)
+#             if (node1.left == None and node2.right != None) or (node1.right == None and node2.left != None):
+#                 return False
+#             if node1.left != None and node2.right != None:
+#                 if node1.left.val != node2.right.val: return False
+#                 queue.append(node1.left)
+#                 queue.append(node2.right)
+#             if node1.right != None and node2.left != None:
+#                 if node1.right.val != node2.left.val: return False
+#                 queue.append(node1.right)
+#                 queue.append(node2.left)
+#         return True
