53ff620128e9b99ee99e1a061395ba2171c7311d
cgerald-yamasaki <94801618+cgerald-yamasaki@users.noreply.github.com>
2023-03-25 16:15:45 -0700

    first working sol

--- 14-Longest_Common_Prefix.py
+++ 14-Longest_Common_Prefix.py
@@ -1,1 +1,20 @@
+# 14. Longest Common Prefix
 
+# Write a function to find the longest common prefix string
+# amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+
+# works, 13.87th percentile for runtime, 79.5th for memory
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        pref = strs[0]
+        for s in strs:
+            if pref == "": return pref
+            if len(s) < len(pref): pref = pref[:len(s)]
+            for l_in in range(len(s)):
+                if l_in >= len(pref):
+                    break
+                if s[l_in] != pref[l_in]:
+                    pref = pref[:l_in]
+                    break
+        return pref
