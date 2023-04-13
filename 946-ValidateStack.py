8d3e7f7a3018897c3be42ab70ab0e969a361837e
cgerald-yamasaki <94801618+cgerald-yamasaki@users.noreply.github.com>
2023-04-13 10:30:52 -0700

    first working sol

--- 946-ValidateStack.py
+++ 946-ValidateStack.py
@@ -1,1 +1,16 @@
+# 946. Validate Stack Sequences
 
+# works, medium fast, fairly low memory (in comparison to other answers)
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        curr = None
+        while popped != []:
+            curr = popped[0]
+            if stack == [] or curr != stack[-1]:
+                if pushed == []: return False
+                stack.append(pushed.pop(0))
+            else: # first el of popped is same as last el of stack
+                stack.pop()
+                popped.pop(0)
+        return True
