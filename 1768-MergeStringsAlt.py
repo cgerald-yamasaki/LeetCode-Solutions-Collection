ce670e8c2e44954a41644f6b27293309e8afccb9
cgerald-yamasaki <94801618+cgerald-yamasaki@users.noreply.github.com>
2023-04-18 15:16:06 -0700

    first two ideas

--- 1768-MergeStringsAlt.py
+++ 1768-MergeStringsAlt.py
@@ -1,1 +1,20 @@
+# 1768. Merge Strings Alternately
 
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        new = ""
+        r = min(len(word1), len(word2))
+        for l in range(r):
+            new = new + word1[l] + word2[l]
+        new = new + word1[r:] + word2[r:]
+        return new
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        new = ""
+        while word1 != "" and word2 != "":
+            new = new + word1[0] + word2[0]
+            word1 = word1[1:]
+            word2 = word2[1:]
+        new = new + word1 + word2
+        return new
--- 946-ValidateStack.py
+++ 946-ValidateStack.py
@@ -1,5 +1,20 @@
 # 946. Validate Stack Sequences
 
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        curr = popped[0]
+        while popped != []:
+            if stack == [] or curr != stack[-1]:
+                if pushed == []: return False
+                stack.append(pushed.pop(0))
+            else: # first el of popped is same as last el of stack
+                stack.pop()
+                popped.pop(0)
+                try: curr = popped[0]
+                except: return True
+        return True
+
 # works, medium fast, fairly low memory (in comparison to other answers)
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
